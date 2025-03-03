f97d4275e408e1b1ff4de54e0910f58a1dfdc0f8
RPedreira <rnpedreira4@gmail.com>
2025-03-03 16:32:48 -0300

    feat: add methods to PokedexController and PokedexService for filtering and catching Pokémon

--- apps/backend/src/controllers/pokedex-controller.ts
+++ apps/backend/src/controllers/pokedex-controller.ts
@@ -1,9 +1,19 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { PokemonInterface } from '@internal/database'
 import { PokedexService } from '../services/pokedex-service'
 
 class PokedexController {
-    static async getAllPokemons(request: FastifyRequest, reply: FastifyReply) {
+    static async getAllPokemons(): Promise<PokemonInterface[]> {
         return PokedexService.getAllPokemons()
+    }
+
+    static async getPokemons(
+        filter: Record<string, unknown>
+    ): Promise<PokemonInterface[]> {
+        return PokedexService.getPokemons(filter)
+    }
+
+    static async caughtPokemon(name: string): Promise<PokemonInterface> {
+        return PokedexService.caughtPokemon(name)
     }
 }
 
--- apps/backend/src/services/pokedex-service.ts
+++ apps/backend/src/services/pokedex-service.ts
@@ -1,13 +1,30 @@
-import {
-    Pokemon,
-    Group,
-    PokemonInterface,
-    GroupInterface
-} from '@internal/database'
+import { Pokemon, PokemonInterface } from '@internal/database'
+import { JoeyQuestService } from './joey-quest-service'
 
 class PokedexService {
-    static async getAllPokemons() {
+    static async getAllPokemons(): Promise<PokemonInterface[]> {
         return await Pokemon.find({}, { pokedexNumber: 1 })
+    }
+
+    static async getPokemons(
+        filter: Record<string, unknown>
+    ): Promise<PokemonInterface[]> {
+        return await Pokemon.find(filter, { pokedexNumber: 1 })
+    }
+
+    static async caughtPokemon(name: string): Promise<PokemonInterface> {
+        const newPokemonRegister = await Pokemon.findOneAndUpdate(
+            { name },
+            { caught: true }
+        )
+
+        if (newPokemonRegister.joeyQuestClans.length) {
+            await JoeyQuestService.updatePokemonGroups(
+                newPokemonRegister.joeyQuestClans
+            )
+        }
+
+        return newPokemonRegister
     }
 }
 
