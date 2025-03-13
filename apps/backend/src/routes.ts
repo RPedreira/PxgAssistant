3625755bc6a2fc9c635ab93f11a981cc125dd0b8
RPedreira <rnpedreira4@gmail.com>
2025-03-13 18:41:08 -0300

    feat: add error handler and new routes for Pokémon and Joey Quest groups

--- apps/backend/src/routes.ts
+++ apps/backend/src/routes.ts
@@ -12,7 +12,26 @@ export async function routes(
     fastify: FastifyInstance,
     options: FastifyPluginOptions
 ) {
-    fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-        return PokedexController.getAllPokemons()
-    })
+    fastify.get(
+        '/pokemons',
+        async (request: FastifyRequest, reply: FastifyReply) => {
+            return PokedexController.getAllPokemons()
+        }
+    )
+
+    fastify.get(
+        '/groups',
+        async (request: FastifyRequest, reply: FastifyReply) => {
+            return JoeyQuestController.getJoeyQuestGroups()
+        }
+    )
+
+    fastify.post(
+        '/pokemons',
+        async (request: FastifyRequest, reply: FastifyReply) => {
+            return PokedexController.getPokemons(
+                request.body as Record<string, unknown>
+            )
+        }
+    )
 }
